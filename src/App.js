1230523aa6f3d95e4e7636234d6ff842b50d0fea
Philip <p208p2002@gmail.com>
2019-10-22 16:20:19 +0800

    no message

--- src/App.js
+++ src/App.js
@@ -1,26 +1,101 @@
 import React from 'react';
-import logo from './logo.svg';
+import posed from 'react-pose';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const randRange = (max,min=1)=>{
+  return Math.random() * (max - min) + min;
 }
 
-export default App;
+const Box = posed.div({
+  hidden: { 
+    opacity: 0, 
+    scale: 0.01, 
+    transition: () => ({ delay: randRange(1000),duration: 500 })
+    },
+  visible: { 
+    opacity: 1, 
+    scale: 1.0, 
+    transition: () => ({ delay: randRange(1000),duration: 500 })
+   },
+});
+
+class Example extends React.Component {
+  state = {
+    isVisible: true,
+    boxsNumbes: 15,
+    xs:[],
+    ys:[],
+  };
+
+  componentDidMount() {
+    setInterval(() => {
+      let { R } = this.props
+      let {isVisible,boxsNumbes} = this.state
+      let xs = []
+      let ys = []
+      
+      for(var i=0;i<boxsNumbes;i++){
+        xs.push(randRange(window.innerWidth,220)-220)
+        ys.push(randRange(window.innerHeight,40)-40)        
+      }
+
+      if(!R){
+        if(isVisible===true){
+          this.setState({
+            isVisible: !this.state.isVisible,
+          });
+        }
+        else{
+          this.setState({
+            isVisible: !this.state.isVisible,          
+            xs,
+            ys,
+          });
+        }
+      }
+      else{
+        if(isVisible!==true){
+          this.setState({
+            isVisible: !this.state.isVisible,
+          });
+        }
+        else{
+          this.setState({
+            isVisible: !this.state.isVisible,          
+            xs,
+            ys,
+          });
+        }
+      }
+      
+    }, 2500);
+  }
+
+  boxs(){
+    let { R } = this.props
+    const { isVisible,boxsNumbes,xs,ys } = this.state;
+    let boxs = []
+    for(var i =0;i<boxsNumbes;i++){
+      boxs.push(<Box
+        key={i}
+        style={{
+          position:'absolute',
+          left:`${xs[i]}px`,
+          top:`${ys[i]}px`
+        }}
+        className="box" 
+        pose={R?isVisible ? 'hidden' : 'visible':isVisible ? 'visible' : 'hidden'} />)
+    }
+    return boxs
+  }
+  
+  render() {
+    return (
+      <React.Fragment>
+        {this.boxs()}
+      </React.Fragment>
+    )
+  }
+}
+
+export default Example
--- src/index.js
+++ src/index.js
@@ -4,7 +4,30 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <div style={{
+        position: 'relative',
+        height: '100%',
+        width: '100%',
+        // padding:500,
+        // paddingBottom:100
+    }}>
+        <div
+            align="center"
+            style={{
+                position: 'absolute',
+                right: '5%',
+                bottom: '5%',
+                zIndex: 1,
+                textShadow:'1px 1px white'               
+            }}>
+            <h1 style={{margin:0}}>Querator AI</h1>
+            <span>present by UDIC</span>
+        </div>
+
+        <App key={"1"} R={true} />
+        <App key={"1"} R={false} />
+    </div>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
