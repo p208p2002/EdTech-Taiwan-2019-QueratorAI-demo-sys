5ef5cf7df42080f29cc1c66df619e1c02867c956
Philip <p208p2002@gmail.com>
2019-11-27 11:01:25 +0800

    box control

--- src/models/QGWallModel/index.js
+++ src/models/QGWallModel/index.js
@@ -2,88 +2,99 @@ import React, { Component } from 'react';
 import Box from './Box.js';
 import cool_data from '../../asset/cool_data.json'
 
-const randRange = (max, min = 1) => {
-    return parseInt(Math.random() * (max - min) + min);
-}
-
 let boxOffset = 10
 let boxHeight = 45 + boxOffset
 let numberOfBoxs = parseInt((window.innerHeight) / (boxHeight)) - 1
 let marginTop = parseInt((window.innerHeight - numberOfBoxs * boxHeight) / 2)
 
+const randRange = (max, min = 1) => {
+	return parseInt(Math.random() * (max - min) + min);
+}
+
 class Manager extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            dataStack: []
-        };
-        this.boxs = []
-        this.boxRefs = []
-        this.questions = cool_data
-        this.connectSocket = this.connectSocket.bind(this)
-        this.fiilDataStack = this.fiilDataStack.bind(this)
-    }
+	constructor(props) {
+		super(props);
+		this.state = {
+			dataStack: []
+		};
+		this.boxs = []
+		this.boxRefs = []
+		this.connectSocket = this.connectSocket.bind(this)
+		this.fiilDataStack = this.fiilDataStack.bind(this)
+		this.setBox = this.setBox.bind(this)
+		this.getRandomCoolData = this.getRandomCoolData.bind(this)
+		this.updateBoxDataFromDataStack = this.updateBoxDataFromDataStack.bind(this)
+	}
 
-    fiilDataStack() {
-      // 使用靜態資源填充
-      let { dataStack } = this.state
-      for(var i=0;i<numberOfBoxs;i++){
-        dataStack.unshift(cool_data[randRange(cool_data.length-1,0)])
-      }
-      console.log(dataStack)
-      this.setState({
-        dataStack
-      })
-    }
+	getRandomCoolData() {
+		return cool_data[randRange(cool_data.length - 1, 0)]
+	}
 
-    connectSocket() {
-      setInterval(()=>{
-        var { dataStack } = this.state
-        var newdata = cool_data[randRange(cool_data.length-1,0)]
-        dataStack.pop()
-        dataStack.unshift(newdata)
-        this.setState({
-          dataStack
-        })
-        console.log(dataStack)
-      },1000)
-    }
+	setBox(refId, option = {}) {
+		let { text = '', isShow = true } = option
+		setTimeout(()=>{
+			this.boxRefs[refId].setText(text)
+			.then(() => {
+				this.boxRefs[refId].show(isShow)
+			})
+		},randRange(350,0))
+	}
 
-    UNSAFE_componentWillMount() {
-        for (var i = 0; i < numberOfBoxs; i++) {
-            this.boxs.push(<Box ref={(input) => { this.boxRefs.push(input) }} key={i} y={i * boxHeight} />)
-        }
-    }
+	fiilDataStack() {
+		// 使用靜態資源填充
+		let { dataStack } = this.state
+		for (var i = 0; i < numberOfBoxs; i++) {
+			dataStack.unshift(this.getRandomCoolData())
+		}
+		console.log(dataStack)
+		this.setState({
+			dataStack
+		})
+	}
 
-    componentDidMount() {
-        this.fiilDataStack()
-        this.connectSocket()
-        setTimeout(() => {
-            this.boxRefs[0].setText('true')
-                .then(() => {
-                    this.boxRefs[0].show(true)
-                })
-        }, 2000)
+	connectSocket() {
+		setInterval(() => {
+			var { dataStack } = this.state
+			var newdata = this.getRandomCoolData()
+			dataStack.pop()
+			dataStack.unshift(newdata)
+			this.setState({
+				dataStack
+			})
+			console.log(dataStack)
+		}, 1000)
+	}
 
-        setTimeout(() => {
-            this.boxRefs[0].setText('true2')
-                .then(() => {
-                    this.boxRefs[0].show(true)
-                })
-        }, 6000)
+	UNSAFE_componentWillMount() {
+		for (var i = 0; i < numberOfBoxs; i++) {
+			this.boxs.push(<Box ref={(input) => { this.boxRefs.push(input) }} key={i} y={i * boxHeight} />)
+		}
+	}
 
+	componentDidMount() {
+		this.fiilDataStack()
+		this.connectSocket()
+		this.updateBoxDataFromDataStack()
+		setInterval(()=>{
+			this.updateBoxDataFromDataStack()
+		},7000)
+	}
 
+	updateBoxDataFromDataStack(){
+		let { dataStack } = this.state
+		for (var i = 0; i < numberOfBoxs; i++) {
+			this.setBox(i, { text: dataStack[i] })
+		}
+	}
 
-    }
-
-    render() {
-        let { boxs } = this
-        return (
-            <div style={{ marginTop }}>
-                {boxs}
-            </div>
-        );
-    }
+	render() {
+		let { boxs } = this
+		return (
+			<div style={{ marginTop }}>
+				{boxs}
+			</div>
+		);
+	}
 }
 
 export default Manager;
